5a491ad6e6d556c536007eb4b94090369a4f0d24
kamilkieruzal <kamil.kieruzal@gmail.com>
2024-07-08 17:48:21 +0200

    Renamed folder

---
+++ ExportReport/DAL/ExportsContext.cs
@@ -0,0 +1,10 @@
+﻿using ExportReport.Models;
+using System.Data.Entity;
+
+namespace ExportReport.DAL
+{
+    public class ExportsContext : DbContext
+    {
+        public DbSet<Export> Exports { get; set; }
+    }
+}
---
+++ ExportReport/Models/Export.cs
@@ -0,0 +1,11 @@
+﻿namespace ExportReport.Models
+{
+    public class Export
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateTime { get; set; }
+        public string UserName { get; set; }
+        public string Location { get; set; }
+    }
+}
